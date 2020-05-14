2b7e30c32cf1550206af0c4c0a824e40b968df1d
kasi-iyo <snakeman143@gmail.com>
2020-05-14 10:59:54 +0900

    no message

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,28 @@
+class SessionsController < ApplicationController
+  skip_before_action :login_required
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    user = User.find_by(email: sessions_params[:email])
+
+    if user&.authenticate(sessions_params[:password]) #ぼっち演算子を付与してnilを返すようにする。
+      session[:user_id] = user.id
+      redirect_to root_url, notice: 'ログインしました。'
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    reset_session
+    redirect_to root_url, notice: 'ログアウトしました。'
+  end
+
+  private
+    def sessions_params
+      params.require(:user).permit(:email, :password)
+    end
+end
