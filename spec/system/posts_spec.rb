3854cd5b8945693a9ce114974bd8d50437304613
kasi-iyo <snakeman143@gmail.com>
2020-05-15 22:10:27 +0900

    【投稿機能テスト】新規投稿、一覧表示、詳細表示。

--- spec/system/posts_spec.rb
+++ spec/system/posts_spec.rb
@@ -1,22 +1,65 @@
 require 'rails_helper'
 
 describe '投稿管理機能', type: :system do
+  let(:user_a) { FactoryBot.create(:user, name: 'ユーザーA', email: 'a@example.com') }
+  let!(:post_a) { FactoryBot.create(:post, title: '最初の投稿', user: user_a) }
+
+  before do
+    visit login_path
+    fill_in 'メールアドレス', with: login_user.email
+    fill_in 'パスワード', with: login_user.password
+    click_button 'ログイン'
+  end
+
+  shared_examples_for 'ユーザーAが作成したタスクが表示される' do
+    it { expect(page).to have_content '最初の投稿' }
+  end
+
   describe '一覧表示機能' do
+    context 'ユーザーAがログインしている時' do
+      let(:login_user) { user_a }
+
+      it_behaves_like 'ユーザーAが作成したタスクが表示される'
+    end
+  end
+
+  describe '詳細表示機能' do
+    context 'ユーザーAがログインしている時' do
+      let(:login_user) { user_a }
+
+      before do
+        visit post_path(post_a)
+      end
+
+      it_behaves_like 'ユーザーAが作成したタスクが表示される'
+    end
+  end
+
+  describe '新規投稿機能' do
+    let(:login_user) { user_a }
+
     before do
-      user_a = FactoryBot.create(:user, name: 'ユーザーA', email: 'a@example.com')
-      FactoryBot.create(:post, title: '最初の投稿', user: user_a)
+      visit new_post_path
+      fill_in 'タイトル', with: post_title
+      click_button '登録する'
     end
-    it 'ユーザーAが作成した投稿が表示される' do
-      expect(page).to have_content '最初の投稿'
+
+    context '新規投稿画面でタイトルを入力した時' do
+      let(:post_title) { '新規投稿のテストを書く' }
+
+      it '正常に登録される' do
+        expect(page).to have_selector '.alert-success', text: '投稿が完了しました'
+      end
     end
-    # context 'ユーザーAがログインしている時' do
-    #   before do
-    #     visit login_path
-    #     fill_in 'メールアドレス', with: 'a@example.com'
-    #     fill_in 'パスワード', with: 'password'
-    #     click_button 'ログイン'
-    #   end
-    #
-    # end
+
+    context '新規投稿画面でタイトルを入力しなかった時' do
+      let(:post_title) { '' }
+
+      it 'エラーとなる' do
+        within '#error_explanation' do
+          expect(page).to have_content 'タイトルを入力してください'
+        end
+      end
+    end
   end
 end
