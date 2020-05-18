5e8bb8fd9f838c9c78250b8e2e7a6cd06ba2f210
kasi-iyo <snakeman143@gmail.com>
2020-05-18 15:18:25 +0900

    検索機能における、検索条件を絞る制御を追加。

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,4 +5,14 @@ class Post < ApplicationRecord
   belongs_to :user
 
   scope :recent, -> { order(created_at: :desc) }
+
+  # カラムについての検索条件を固定
+  def self.ransakable_attributes(auth_object = nil)
+    %w[name created_at]
+  end
+
+  # 検索条件に意図しない関連を含めないようにする
+  def self.ransakable_associations(auth_object = nil)
+    []
+  end
 end
