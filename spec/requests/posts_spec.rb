85e9f31fbd5e560adf820e07b88b8e472fd28469
kasi-iyo <snakeman143@gmail.com>
2020-05-26 16:52:16 +0900

    Request specsでpostをテスト

---
+++ spec/requests/posts_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+RSpec.describe "Posts", type: :request do
+
+  let(:user_a) { FactoryBot.create(:user) }
+  let(:login_user) { login(user_a) }
+  let(:post_a) { FactoryBot.create(:post) }
+  let(:new_post) { FactoryBot.attributes_for(:post) }
+
+
+  describe "newアクション" do
+    it "正常なレスポンスを返すこと" do
+      get posts_path
+      expect(response).to be_successful
+      expect(response).to have_http_status "200"
+    end
+  end
+
+  describe "createアクション" do
+
+    context "認証済みのユーザーの場合" do
+
+      context "有効な値の場合" do
+
+        it "投稿を作成できること" do
+
+        end
+      end
+
+      context "無効な値の場合" do
+
+      end
+    end
+
+    context "ゲストの場合" do
+    end
+  end
+end
